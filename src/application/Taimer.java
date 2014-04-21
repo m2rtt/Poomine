511cc209bac41a59ae92780c91e7f031883a3648
m2rtt <m2rt.sessman@gmail.com>
2014-04-21 17:51:13 +0300

    taimer

---
+++ src/application/Taimer.java
@@ -0,0 +1,42 @@
+package application;
+
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Taimer {
+	static int intervall;
+	static Timer taimer;
+	static int aeg = 10; 
+	
+	public static void main(String[] args) {
+		alusta();
+	}
+	public static void alusta() {
+		int delay = 1000;
+		int period = 1000;
+		taimer = new Timer();
+		intervall = getAeg();
+		// google'i abiga loodud taimer
+		taimer.scheduleAtFixedRate(new TimerTask() {
+
+			public void run() {
+				System.out.println(setIntervall());
+
+			}
+		}, delay, period);
+	}
+
+	private static final int setIntervall() {
+		if (intervall == 1) {
+			taimer.cancel();
+		}
+		return --intervall;
+	}
+	public void setAeg(int aeg) {
+		this.aeg = aeg;
+	}
+	public static int getAeg() {
+		return aeg;
+	}
+}
