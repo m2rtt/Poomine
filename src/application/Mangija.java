fb9e54a2a55985f02884903821eaa415da8bd336
m2rtt <m2rt.sessman@gmail.com>
2014-04-21 17:27:33 +0300

    Mangija

---
+++ src/application/Mangija.java
@@ -0,0 +1,15 @@
+package application;
+
+public class Mangija {
+	int skoor = 0;
+
+	public Mangija() {
+		
+	}
+	int getSkoor() {
+		return skoor;
+	}
+	void setSkoor(int skoor) {
+		this.skoor = skoor;
+	}
+}
