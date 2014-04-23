10f7e788e423236655e35c96c263a9fa13fb22cf
marvinmaran <marvinmaran100@gmail.com>
2014-04-23 14:52:56 +0300

    Revert "Revert "Revert "vb"""
    This reverts commit d7dc5f7a366d81e254f67ce37e6aeea176231c5d.

---
+++ src/application/Faililugeja.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.io.FileNotFoundException;
+
+public class Faililugeja extends Main {
+
+	public static void faililugeja() throws FileNotFoundException {
+
+		java.io.File fail = new java.io.File("src\\application\\sõnad.txt");
+		java.util.Scanner sc = new java.util.Scanner(fail);
+		int counter = 0;
+		sõnalistt.removeAll(sõnalistt); // enne teeme sõnalisti tühjaks
+		while (sc.hasNextLine()) {
+			String rida = sc.nextLine();
+			//System.out.println(rida);
+			String[] sõnavihje = rida.split(";"); // teeme pooleks sõna ja vihje
+			sõnalistt.add(sõnavihje[0]);
+			vihjelist.add(sõnavihje[1]);
+			System.out.println(sõnalistt.get(counter) + " | " + vihjelist.get(counter)); // prindib konsooli sõnad ja nende vihjed
+			counter += 1;
+			
+		}
+		sc.close();
+	}
+}
